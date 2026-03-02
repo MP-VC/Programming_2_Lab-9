6abfa6e258fd969049a962173b0456f55af99202
MP-VC <2519989@edu.vaniercollege.qc.ca>
2026-03-01 21:00:17 -0500

    did customer and discountrate

--- Q2/Customer.java
+++ Q2/Customer.java
@@ -1,5 +1,32 @@
 package Q2;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Customer {
-    
+    String name;
+    boolean member = false;
+    String memberType = "none";
+    ArrayList<String> validType = new ArrayList<>(Arrays.asList("premium","gold","silver"));
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public void setMember() {
+        member = true;
+    }
+    public void setMemberType(String type) {
+        if(validType.contains(type.toLowerCase()) && member) memberType = type;
+    }
+
+    public String getMemberType() {
+        return memberType;
+    }
+
+    @Override
+    public String toString() {
+        return  "Name: " + name + '\n' +
+                "Member: " + member + '\n' +
+                "Member type: " + memberType;
+    }
 }
--- Q2/DiscountRate.java
+++ Q2/DiscountRate.java
@@ -1,5 +1,47 @@
 package Q2;
 
 public class DiscountRate {
+
     
+    double serviceDiscountPremium = 0.2;
+    double serviceDiscountGold = 0.15;
+    double serviceDiscountSilver = 0.1;
+
+    double productDiscountPremium = 0.1;
+    double productDiscountGold = 0.1;
+    double productDiscountSilver = 0.1;
+
+    public double getServiceDiscountRate(String type) {
+        switch (type.toUpperCase()) {
+
+            case "PREMIUM":
+                return serviceDiscountPremium;
+
+            case "GOLD":
+                return serviceDiscountGold;
+
+            case "SILVER":
+                return serviceDiscountSilver;
+            
+            default:
+                return 0;
+        }
+    }
+
+    public double getProductDiscountRate(String type) {
+        switch (type.toUpperCase()) {
+
+            case "PREMIUM":
+                return productDiscountPremium;
+
+            case "GOLD":
+                return productDiscountGold;
+
+            case "SILVER":
+                return productDiscountSilver;
+            
+            default:
+                return 0;
+        }
+    }
 }
